193bc7d573d61cde84b78d058d65bc755c7d6f35
venesa okuna <venesa.okuna@student.moringaschool.com>
2021-11-11 15:51:15 +0300

    This commit will add subtraction function to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// addition function
+
 var add = function(number1, number2) {
     return number1 + number2;
   };
@@ -6,3 +8,16 @@ var add = function(number1, number2) {
   var number2 = parseInt(prompt("Enter another number:"));
   var result = add(number1, number2);
   alert(result);
+
+// subtract function
+
+var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = subtract(number1, number2);
+  alert(result);
+
+
